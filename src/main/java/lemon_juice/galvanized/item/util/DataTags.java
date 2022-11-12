bc54ad74262248bc51b31941f869dbbb6e10d126
Lemon-Juiced <tbrotheawesome@gmail.com>
2022-11-12 01:14:24 -0500

    Fixed Bores

--- src/main/java/lemon_juice/galvanized/item/util/DataTags.java
+++ src/main/java/lemon_juice/galvanized/item/util/DataTags.java
@@ -6,7 +6,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class DataTags {
-    public static final TagKey<Block> WITH_BORE = BlockTags.create(new ResourceLocation("minecraft:mineable/bore.json"));
+    public static final TagKey<Block> WITH_BORE = BlockTags.create(new ResourceLocation("minecraft:mineable/bore"));
     public static final TagKey<Block> WITH_EXCAVATOR = BlockTags.create(new ResourceLocation("minecraft:mineable/excavator"));
     public static final TagKey<Block> WITH_HAMMER = BlockTags.create(new ResourceLocation("minecraft:mineable/hammer"));
     public static final TagKey<Block> WITH_LUMBERAXE = BlockTags.create(new ResourceLocation("minecraft:mineable/lumberaxe"));
